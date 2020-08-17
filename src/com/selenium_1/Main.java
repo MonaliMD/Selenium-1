0a573ae14e5f587475cf9cccd1f8db17b9d5ce73
Monali Wijesinghe <monalimeg@gmail.com>
2020-08-17 16:08:11 +1200

    Add test script for "Find element" try out

--- src/com/selenium_1/Main.java
+++ src/com/selenium_1/Main.java
@@ -1,8 +1,24 @@
 package com.selenium_1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 	// write your code here
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.com/s?k=headset&ref=nb_sb_noss_2");
+
+        //How to use Find Element command
+        driver.findElement(By.id("a-autoid-0")).click();
+
+        driver.findElement(By.id("s-result-sort-select_1")).click();
+
+        Thread.sleep(3000);
+        driver.close();
     }
 }
